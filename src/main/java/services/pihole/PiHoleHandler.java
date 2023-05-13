936d1589c6391f23cc314fb8d9842ea9858bc272
Kitsune <fox-inflames@outlook.com>
2023-05-13 22:20:08 +0100

    -Optimised API class and removed useless code.

--- src/main/java/services/pihole/PiHoleHandler.java
+++ src/main/java/services/pihole/PiHoleHandler.java
@@ -24,29 +24,21 @@ import domain.pihole.TopAd;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import services.helpers.HelperService;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PiHoleHandler {
 
     private final String IPAddress;
     private final int Port;
     private final String Auth;
-
-    private HttpURLConnection conn;
-    private InputStreamReader in;
-    private BufferedReader br;
-
-    private JSONObject jsonResult;
-    private JSONParser parser;
-    private String output;
-
-    private int responseCode = 0;
 
 
     public PiHoleHandler(String IPAddress, int Port, String auth) {
@@ -55,184 +47,118 @@ public class PiHoleHandler {
         this.Auth = auth;
     }
 
+    public String getIPAddress() {
+        return IPAddress;
+    }
+
     public PiHole getPiHoleStats() {
 
-        if (!initAPI("summary", ""))
-            return null;
+        String output = getApiResponseAsString("summary", "");
+        JSONParser parser = new JSONParser();
+        JSONObject jsonResult = null;
+        if (output != null && output.equals("")) return null;
 
         // Transform Raw result to JSON
 
         try {
-            in = new InputStreamReader(conn.getInputStream());
 
-            br = new BufferedReader(in);
-
-            output = br.readLine();
-            parser = new JSONParser();
             jsonResult = (JSONObject) parser.parse(output);
 
 
-        } catch (IOException | ParseException ioe) {
+        } catch (ParseException ioe) {
             ioe.printStackTrace();
         }
 
+        if (jsonResult != null) {
+            JSONObject gravity_json = (JSONObject) jsonResult.get("gravity_last_updated");
+            JSONObject relative_json = (JSONObject) gravity_json.get("relative");
 
-        JSONObject gravity_json = (JSONObject) jsonResult.get("gravity_last_updated");
-        JSONObject relative_json = (JSONObject) gravity_json.get("relative");
+            boolean file_exists = (boolean) gravity_json.get("file_exists");
+            Long absolute = (Long) gravity_json.get("absolute");
+            Long days = (Long) relative_json.get("days");
+            Long hours = (Long) relative_json.get("hours");
+            Long minutes = (Long) relative_json.get("minutes");
 
-        boolean file_exists = (boolean) gravity_json.get("file_exists");
-        Long absolute = (Long) gravity_json.get("absolute");
-        Long days = (Long) relative_json.get("days");
-        Long hours = (Long) relative_json.get("hours");
-        Long minutes = (Long) relative_json.get("minutes");
-
-        Gravity gravity = new Gravity(file_exists, absolute, days, hours, minutes);
-        try {
-
-            /*
-            String bigNumber = "1,234,567,899";
-            NumberFormat format = NumberFormat.getInstance(Locale.US);
-            Number number = 0;
+            Gravity gravity = new Gravity(file_exists, absolute, days, hours, minutes);
             try {
-                number = format.parse(bigNumber);
-            } catch (ParseException e) {
-                e.printStackTrace();
+
+                Long domains_being_blocked = Long.parseLong(HelperService.convertJsonToLong(jsonResult.get("domains_being_blocked")));
+                Long dns_queries_today = Long.parseLong(HelperService.convertJsonToLong(jsonResult.get("dns_queries_today")));
+                Long ads_blocked_today = Long.parseLong(HelperService.convertJsonToLong(jsonResult.get("ads_blocked_today")));
+                Double ads_percentage_today = Double.parseDouble(HelperService.convertJsonToLong(jsonResult.get("ads_percentage_today")));
+                Long unique_domains = Long.parseLong(HelperService.convertJsonToLong(jsonResult.get("unique_domains")));
+                Long queries_forwarded = Long.parseLong(HelperService.convertJsonToLong(jsonResult.get("queries_forwarded")));
+                Long queries_cached = Long.parseLong(HelperService.convertJsonToLong(jsonResult.get("queries_cached")));
+                Long clients_ever_seen = Long.parseLong((String) jsonResult.get("clients_ever_seen"));
+                Long unique_clients = Long.parseLong((String) jsonResult.get("unique_clients"));
+                Long dns_queries_all_types = Long.parseLong(HelperService.convertJsonToLong(jsonResult.get("dns_queries_all_types")));
+                Long reply_NODATA = Long.parseLong(HelperService.convertJsonToLong(jsonResult.get("reply_NODATA")));
+                Long reply_NXDOMAIN = Long.parseLong(HelperService.convertJsonToLong(jsonResult.get("reply_NXDOMAIN")));
+                Long reply_CNAME = Long.parseLong(HelperService.convertJsonToLong(jsonResult.get("reply_CNAME")));
+                Long reply_IP = Long.parseLong(HelperService.convertJsonToLong(jsonResult.get("reply_IP")));
+                Long privacy_level = Long.parseLong((String) jsonResult.get("privacy_level"));
+                String status = (String) jsonResult.get("status");
+
+
+                return new PiHole(domains_being_blocked, dns_queries_today, ads_blocked_today, ads_percentage_today, unique_domains, queries_forwarded, queries_cached, clients_ever_seen, unique_clients, dns_queries_all_types, reply_NODATA, reply_NXDOMAIN, reply_CNAME, reply_IP, privacy_level, status, gravity);
+            } catch (NumberFormatException nfe) {
+                System.out.println("NumberFormatException: " + nfe.getMessage());
+                return null;
             }
-            long result = number.longValue();
-
-             */
-
-        Long domains_being_blocked = Long.parseLong(convertJsonToLong(jsonResult.get("domains_being_blocked")));
-        Long dns_queries_today = Long.parseLong(convertJsonToLong(jsonResult.get("dns_queries_today")));
-        Long ads_blocked_today = Long.parseLong(convertJsonToLong( jsonResult.get("ads_blocked_today")));
-        Double ads_percentage_today= Double.parseDouble(convertJsonToLong(jsonResult.get("ads_percentage_today")));
-        Long unique_domains = Long.parseLong(convertJsonToLong( jsonResult.get("unique_domains")));
-        Long queries_forwarded = Long.parseLong(convertJsonToLong( jsonResult.get("queries_forwarded")));
-        Long queries_cached = Long.parseLong(convertJsonToLong( jsonResult.get("queries_cached")));
-        Long clients_ever_seen = Long.parseLong((String) jsonResult.get("clients_ever_seen"));
-        Long unique_clients = Long.parseLong((String)  jsonResult.get("unique_clients"));
-        Long dns_queries_all_types = Long.parseLong(convertJsonToLong( jsonResult.get("dns_queries_all_types")));
-        Long reply_NODATA = Long.parseLong(convertJsonToLong( jsonResult.get("reply_NODATA")));
-        Long reply_NXDOMAIN = Long.parseLong(convertJsonToLong( jsonResult.get("reply_NXDOMAIN")));
-        Long reply_CNAME = Long.parseLong(convertJsonToLong(  jsonResult.get("reply_CNAME")));
-        Long reply_IP = Long.parseLong(convertJsonToLong( jsonResult.get("reply_IP")));
-        Long privacy_level = Long.parseLong((String)  jsonResult.get("privacy_level"));
-        String status = (String) jsonResult.get("status");
-
-
-        return new PiHole(domains_being_blocked, dns_queries_today, ads_blocked_today, ads_percentage_today, unique_domains
-                , queries_forwarded, queries_cached, clients_ever_seen, unique_clients, dns_queries_all_types, reply_NODATA,
-                reply_NXDOMAIN, reply_CNAME, reply_IP, privacy_level, status, gravity);
-        } catch (NumberFormatException nfe) {
-            System.out.println("NumberFormatException: " + nfe.getMessage());
-            return null;
         }
-    }
-
-    private String convertJsonToLong(Object obj)
-    {
-        String objToReturn=(String) obj;
-
-        return objToReturn.replaceAll(",", "").toString();
-
+        return null;
     }
 
     public String getLastBlocked() {
         if (Auth != null && !Auth.isEmpty()) {
-            if (!initAPI("recentBlocked", ""))
-                return "";
+            String output = getApiResponseAsString("recentBlocked", "");
+            if (output != null && output.equals("")) return "";
 
-            // Transform Raw result to JSON
+            return output;
 
-            try {
-                in = new InputStreamReader(conn.getInputStream());
-
-                br = new BufferedReader(in);
-
-                output = br.readLine();
-                parser = new JSONParser();
-                return output;
-
-
-            } catch (IOException e) {
-                e.printStackTrace();
-                return "";
-            }
         } else return "Please verify your Authentication Token";
     }
 
     public String getVersion() {
 
-        if (!initAPI("type%20&%20version", ""))
-            return "";
+        String output = getApiResponseAsString("type%20&%20version", "");
+        if (output != null && output.equals("")) return "";
 
         // Transform Raw result to JSON
 
+        JSONParser parser = new JSONParser();
+        JSONObject jsonResult = null;
         try {
-            in = new InputStreamReader(conn.getInputStream());
-
-            br = new BufferedReader(in);
-
-            output = br.readLine();
-            parser = new JSONParser();
-            try {
-                jsonResult = (JSONObject) parser.parse(output);
-            } catch (ParseException e) {
-                e.printStackTrace();
-            }
-            return jsonResult.get("version").toString();
-
-
-        } catch (IOException e) {
+            jsonResult = (JSONObject) parser.parse(output);
+        } catch (ParseException e) {
             e.printStackTrace();
-            return "";
         }
-
+        if (jsonResult != null) return jsonResult.get("version").toString();
+        return "";
     }
 
     public List<TopAd> getTopXBlocked(int x) {
         if (Auth != null && !Auth.isEmpty()) {
 
+            String output = getApiResponseAsString("topItems", String.valueOf(x));
 
-            if (!initAPI("topItems", String.valueOf(x)))
-                return null;
+            if (output != null && output.equals("")) return null;
 
             // Transform Raw result to JSON
 
+            JSONParser parser = new JSONParser();
             try {
-                in = new InputStreamReader(conn.getInputStream());
 
-                br = new BufferedReader(in);
+                JSONObject jsonResult = (JSONObject) parser.parse(output);
+                JSONObject topADS = (JSONObject) jsonResult.get("top_ads");
+                List<TopAd> list = new ArrayList<>();
 
-                output = br.readLine();
-                parser = new JSONParser();
-                try {
-                    StringBuilder sb = new StringBuilder();
+                topADS.forEach((key, value) -> list.add(new TopAd((String) key, (Long) value)));
 
+                list.sort((s1, s2) -> Long.compare(s2.getNumberBlocked(), s1.getNumberBlocked()));
 
-                    jsonResult = (JSONObject) parser.parse(output);
-                    JSONObject topADS = (JSONObject) jsonResult.get("top_ads");
-                    List<TopAd> list = new ArrayList<>();
-
-                    topADS.forEach((key, value) -> list.add(new TopAd((String) key, (Long) value)));
-
-                    list.sort((s1, s2) -> Long.compare(s2.getNumberBlocked(), s1.getNumberBlocked()));
-
-                    return list;
-                    /*
-                    sb.append("Top ").append(x).append(" blocked: \n\n");
-                    for (TopAd s : list) {
-                        sb.append(s.getDomain()).append(": ").append(s.getNumberBlocked()).append("\n\n");
-                    }
-
-                    return sb.toString();
-                    */
-                } catch (ParseException e) {
-                    e.printStackTrace();
-                }
-                return null;
-
-            } catch (IOException e) {
+                return list;
+            } catch (ParseException e) {
                 e.printStackTrace();
                 return null;
             }
@@ -245,74 +171,68 @@ public class PiHoleHandler {
         if (pihole1 != null) {
             String textToDisplay = "";
             long days = pihole1.getGravity().getDays();
-            if (days <= 1)
-                textToDisplay += days + " day";
-            else
-                textToDisplay += days + " days";
+            if (days <= 1) textToDisplay += days + " day";
+            else textToDisplay += days + " days";
 
             long hours = pihole1.getGravity().getHours();
-            if (hours <= 1)
-                textToDisplay += " " + hours + " hour";
-            else
-                textToDisplay += " " + hours + " hours";
+            if (hours <= 1) textToDisplay += " " + hours + " hour";
+            else textToDisplay += " " + hours + " hours";
 
             long mins = pihole1.getGravity().getMinutes();
-            if (mins <= 1)
-                textToDisplay += " " + mins + " min";
-            else
-                textToDisplay += " " + mins + " mins";
+            if (mins <= 1) textToDisplay += " " + mins + " min";
+            else textToDisplay += " " + mins + " mins";
 
             return textToDisplay;
         }
         return "";
     }
 
-    public String getIPAddress() {
-        return IPAddress;
-    }
 
-    private boolean initAPI(String Param, String ParamVal) {
+    private String getApiResponseAsString(String Param, String ParamVal) {
 
         String fullAuth = "";
         String fullParam = "";
         String fullParamVal = "";
+        InputStreamReader in;
+        BufferedReader br;
 
-        if (!Param.isEmpty())
-            fullParam = "?" + Param;
+        int responseCode;
+
+        if (!Param.isEmpty()) fullParam = "?" + Param;
 
 
-        if (!ParamVal.isEmpty())
-            fullParamVal = "=" + ParamVal;
+        if (!ParamVal.isEmpty()) fullParamVal = "=" + ParamVal;
 
-        if (Auth != null && !Auth.isEmpty())
-            fullAuth = "&auth=" + this.Auth;
+        if (Auth != null && !Auth.isEmpty()) fullAuth = "&auth=" + this.Auth;
 
 
         if (!IPAddress.isEmpty()) {
             try {
                 URL url = new URL("http://" + IPAddress + ":" + Port + "/admin/api.php" + fullParam + fullParamVal + fullAuth);
 
-                conn = (HttpURLConnection) url.openConnection();
-                conn.setConnectTimeout(1000);
-                conn.setReadTimeout(1000);
-                conn.setRequestMethod("GET");
-                conn.setRequestProperty("Accept", "application/json");
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
                 responseCode = conn.getResponseCode();
 
-                if (responseCode != 200) {
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    String server_response;
+                    in = new InputStreamReader(conn.getInputStream());
+                    br = new BufferedReader(in);
+
+                    server_response = br.readLine();
+                    return server_response;
+                } else {
                     System.out.println("Failed : HTTP Error code : " + responseCode);
                     conn.disconnect();
-                    return false;
+                    return "";
                 }
-                return true;
 
             } catch (IOException e) {
                 System.out.println("API Error for:" + IPAddress + " " + e.getMessage());
-                conn.disconnect();
-                return false;
+                return "";
             }
         }
-        return false;
+        return "";
     }
 
 }
